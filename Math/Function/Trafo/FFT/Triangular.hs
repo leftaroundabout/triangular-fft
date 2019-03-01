26a6b86dc48fe48aa2a55d61130982f8e64176dc
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-03-01 11:42:05 +0100

    Prototypes for sampling & evaluation of trangle-functions.

--- Math/Function/Trafo/FFT/Triangular.hs
+++ Math/Function/Trafo/FFT/Triangular.hs
@@ -16,12 +16,15 @@ module Math.Function.Trafo.FFT.Triangular
 
 import Data.Manifold.Types
 import Data.Complex
+import Data.List
 import Data.VectorSpace
 import qualified Linear as Lin
 
 import qualified Data.Vector.Unboxed as UArr
 
 import Control.Monad
+import Control.Applicative
+import Control.Monad.Trans.State
 
 newtype UnitTriangle dims
     = UnitTriangle { getUnitTrianglePoint :: dims Double
@@ -35,17 +38,29 @@ data HomSampledTriangFunction dims s = HomSampledTriangFunction
   , hstfSpan :: dims (dims Double)
   }
 
-evalTriangFunction :: (Foldable dims, Lin.Additive dims)
+evalTriangFunction :: (Foldable dims, Applicative dims, Lin.Additive dims)
      => HomSampledTriangFunction dims s -> UnitTriangle dims -> s
 evalTriangFunction (HomSampledTriangFunction dat resos basept span) (UnitTriangle p)
       = let pInArr = basept Lin.^+^ span Lin.!* p
+            linIndex = snd . (`execState`(1,0)) . forM_ (liftA2 (,) pInArr resos)
+                         $ \(iInDir,n) -> do
+                 (stride, iLin) <- get
+                 put (stride * n, iLin + round iInDir*stride)
         in undefined
 
-sampleTriangle :: (Num s, Traversable dims, Applicative dims, VectorSpace (dims Double))
+sampleTriangle :: ( Num s, UArr.Unbox s
+                  , Traversable dims, Applicative dims, VectorSpace (dims Double) )
          => dims Int -> (UnitTriangle dims -> s) -> HomSampledTriangFunction dims s
 sampleTriangle resos f
     = HomSampledTriangFunction 
-        undefined
+        (UArr.generate (foldl' (*) 1 resos)
+          $ \iLin -> let poss = evalState (traverse (\rHere->do
+                                              iRun <- get
+                                              let (iHere, iRem) = iRun`divMod`rHere
+                                              return $ fromIntegral iHere / fromIntegral rHere
+                                             ) resos) iLin
+                     in if foldr
+                         then f . UnitTriangle $ fmap fromIntegral ixs)
         resos
         zeroV
         Lin.identity
